17e0e572fceaa95b3d2886f380ab60ba6d0766cb
Yan Waipann <yanwaipann@gmail.com>
2022-02-06 12:43:51 +0630

    Add button to my account

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,25 @@
 import React from "react";
 
-import { Pane, Heading, majorScale } from "evergreen-ui";
+import {
+  Avatar,
+  Pane,
+  Heading,
+  Button,
+  Text,
+  majorScale,
+  minorScale,
+} from "evergreen-ui";
+import { useNavigate } from "react-router-dom";
+
+import { useAuth } from "../context/auth-context";
 
 export function Nav() {
+  const {
+    authData: { admin },
+  } = useAuth();
+
+  const navigate = useNavigate();
+
   return (
     <Pane
       display="flex"
@@ -13,8 +30,23 @@ export function Nav() {
       elevation={1}
     >
       <Heading as="h4" size={500} cursor="pointer">
-        Poo Suu Admins
+        {admin ? (
+          <Pane display="flex" alignItems="center">
+            <Avatar src={admin.avatarUrl} name={admin.name} size={40} />
+            <Text marginLeft={minorScale(3)}>{admin.username}</Text>
+          </Pane>
+        ) : (
+          "Poo Suu Admins"
+        )}
       </Heading>
+      {admin && (
+        <Button
+          marginRight={16}
+          onClick={() => navigate("/dashboard/my-account")}
+        >
+          My Account
+        </Button>
+      )}
     </Pane>
   );
 }
