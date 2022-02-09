1dcbb759fcd5a138b0d569ec6d4cd1612ed3c41f
Yan Waipann <yanwaipann@gmail.com>
2022-02-09 15:27:20 +0630

    Increase viewbox size

--- src/components/SongLoader.js
+++ src/components/SongLoader.js
@@ -6,12 +6,12 @@ export const SongLoader = (props) => (
     speed={2}
     width={182}
     height={250}
-    viewBox="0 0 300 400"
+    viewBox="0 0 182 250"
     backgroundColor="#d4d4d4"
     foregroundColor="#ecebeb"
     {...props}
   >
-    <rect x="0" y="0" rx="0" ry="0" width="170" height="170" />
+    <rect x="0" y="0" rx="0" ry="0" width="182" height="170" />
     <rect x="0" y="180" rx="0" ry="0" width="118" height="20" />
     <rect x="0" y="210" rx="0" ry="0" width="141" height="30" />
   </ContentLoader>
