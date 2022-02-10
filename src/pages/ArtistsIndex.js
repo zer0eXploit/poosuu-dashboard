9da379a95d4ff4b0b3c3337e43948875935076ac
Yan Waipann <yanwaipann@gmail.com>
2022-02-10 16:29:19 +0630

    Use SearchWithSuggestions as search box

--- src/pages/ArtistsIndex.js
+++ src/pages/ArtistsIndex.js
@@ -1,77 +1,34 @@
 import React from "react";
 
-import { Pane, Heading, SearchInput, majorScale } from "evergreen-ui";
+import { Link } from "react-router-dom";
+import { Pane, Heading, Text, majorScale } from "evergreen-ui";
 
-import { Container, CardLoader, ArtistCard } from "../components";
+import { Container, ArtistCard, SearchWithSuggestions } from "../components";
 
-import { useAsync } from "../hooks";
-
-import { searchArtists } from "../utils/artists";
+function NoResultInfo() {
+  return (
+    <Pane>
+      <Text size={500}>Unfortunately there is no result. :(</Text>
+      <Text display="block" size={500} marginTop="10px">
+        Your search keyword must be a complete artist name or{" "}
+        <Link to="/dashboard/artists/create">create artist now.</Link>
+      </Text>
+    </Pane>
+  );
+}
 
 export default function ArtistsIndex() {
-  const { status, data, error, run } = useAsync();
-
-  const handleSearch = (e) => {
-    e.preventDefault();
-
-    const term = e.target.elements["term"].value;
-    if (!!!term) return;
-
-    run(searchArtists(term));
-  };
-
   return (
     <Container disableMt>
       <Heading as="h3" size={900} marginBottom={majorScale(3)}>
         Artists
       </Heading>
-      <Pane display="flex" flexDirection="column">
-        <form
-          style={{ width: "100%" }}
-          onSubmit={handleSearch}
-          autoComplete="off"
-        >
-          <SearchInput
-            placeholder="Enter artist name to search"
-            height="50px"
-            width="100%"
-            name="term"
-            disabled={status === "pending"}
-          />
-        </form>
-        <Pane
-          display="flex"
-          gap={majorScale(2)}
-          marginY={majorScale(3)}
-          flexWrap="wrap"
-        >
-          {status === "rejected" && (
-            <Heading as="h4" size={500} marginY={majorScale(5)}>
-              {error?.response?.data?.error ?? "Something went wrong..."}
-            </Heading>
-          )}
-
-          {status === "pending" && (
-            <>
-              <CardLoader />
-              <CardLoader />
-              <CardLoader />
-            </>
-          )}
-
-          {status === "resolved" &&
-            data?.data?.length > 0 &&
-            data.data.map(({ name, image, _id }) => (
-              <ArtistCard name={name} image={image} id={_id} key={_id} />
-            ))}
-          {status === "resolved" && data?.data?.length === 0 && (
-            <Heading as="h4" size={500} marginY={majorScale(5)}>
-              No artist found! Please curate your search to contain at least
-              complete first or last name.
-            </Heading>
-          )}
-        </Pane>
-      </Pane>
+      <SearchWithSuggestions
+        placeholder={"Enter song name"}
+        searchConfig={{ url: "/artists" }}
+        ResultCard={ArtistCard}
+        NoResultInfo={NoResultInfo}
+      />
     </Container>
   );
 }
